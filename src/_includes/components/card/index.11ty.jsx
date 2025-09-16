fb19cf0b68c883ec4f4a35512f2c897a7991e3b6
Dallas Gale <dallasgale.digital@gmail.com>
2025-09-16 19:53:25 +1000

    wip

--- src/_includes/components/card/index.11ty.jsx
+++ src/_includes/components/card/index.11ty.jsx
@@ -5,14 +5,22 @@ import PrimaryLink from "../primaryLink/index.11ty"
 const Card = ({heading, image}) => {
   return (
     <div className="card">
-      <div className="card__img-wrapper">
-        <img src={image} width="100%" height="auto" />
-      </div>
+      <picture className="card__img-wrapper">
+        <img src={image} width="100%" height="auto" className="card__img" />
+      </picture>
 
-      <h2 className="display2 text-white">{heading}</h2>
-      <div className="card__footer">
-        <PrimaryButton label="Enrol" />
-        <PrimaryLink label="Course Overview" />
+      <div className="card__content">
+        <div className="card__content-head">
+          <small className="label3 text-dark-navy">Cyber Security</small>
+          <small className="label3 text-dark-navy">1hr</small>
+        </div>
+        <div className="card__content-title">
+          <h2 className="display2 text-dark-navy">{heading}</h2>
+        </div>
+        {/* <div className="card__footer">
+          <PrimaryButton label="Enrol" />
+          <PrimaryLink label="Course Overview" />
+        </div> */}
       </div>
     </div>
   )
