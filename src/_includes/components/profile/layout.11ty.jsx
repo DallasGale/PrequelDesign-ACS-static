38e22e1ce875af0ce6d82575115cf82f954f94aa
Dallas Gale <dallasgale.digital@gmail.com>
2025-09-17 18:30:37 +1000

    wip

--- src/_includes/components/profile/layout.11ty.jsx
+++ src/_includes/components/profile/layout.11ty.jsx
@@ -3,7 +3,7 @@ import React from "react"
 const ProfileLayout = ({id, title, active, children}) => {
   return (
     <div className={`tab-pane ${active ? "active" : ""}`} id={id}>
-      <div className="profile-layout__content card-bg">
+      <div className="card-bg generic-page">
         <h1 className="display1">{title}</h1>
         <div className="generic-page__content">{children}</div>
       </div>
