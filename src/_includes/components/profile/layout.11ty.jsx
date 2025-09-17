835e54d4137696dde72c564afd1280ab7c256238
Dallas Gale <dallasgale.digital@gmail.com>
2025-09-17 12:59:39 +1000

    wip

--- src/_includes/components/profile/layout.11ty.jsx
+++ src/_includes/components/profile/layout.11ty.jsx
@@ -2,9 +2,9 @@ import React from "react"
 
 const ProfileLayout = ({id, title, active, children}) => {
   return (
-    <div className={`tab-pane card-bg ${active ? "active" : ""}`} id={id}>
+    <div className={`tab-pane ${active ? "active" : ""}`} id={id}>
       <h3 className="display1">{title}</h3>
-      <div className="profile-layout__content">{children}</div>
+      <div className="profile-layout__content card-bg">{children}</div>
     </div>
   )
 }
