f89062195891120d0413aa0ddf4b8fa0c652afdb
Dallas Gale <dallasgale.digital@gmail.com>
2025-09-23 08:53:34 +1000

    wip

--- src/student-dashboard.11ty.jsx
+++ src/student-dashboard.11ty.jsx
@@ -7,7 +7,17 @@ const StudentDashboard = ({title}) => {
   return (
     <Layout>
       <div className="card-bg generic-page">
-        <h1 className="display1">{title}</h1>
+        <h2 className="display1">Enrolled Courses</h2>
+        <div className="generic-page__content mb-5">
+          <div className="card-grid">
+            <Card
+              heading="Cyber security micro-credential for educators"
+              image="/assets/images/course-title-acs.png"
+            />
+          </div>
+        </div>
+
+        <h2 className="display1">Browse Courses</h2>
         <div className="generic-page__content">
           <div className="card-grid">
             <Card
